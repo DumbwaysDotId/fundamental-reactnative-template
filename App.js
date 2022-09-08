0709c0b6336dc5a6cbbdaffc3362e0a2b156915b
Your Name <samsulr99@gmail.com>
2022-09-08 09:27:19 +0700

    7.list-of-map

--- App.js
+++ App.js
@@ -3,13 +3,12 @@ import React from "react";
 import { View } from "react-native";
 
 //Import Screen
-import Map from "./src/screens/Map";
 
 export default function App() {
   return (
-    <View style={{ marginTop: 100 }}>
+    <View>
       <StatusBar />
-      <Map />
+      {/* Use Component */}
     </View>
   );
 }
--- src/screens/Map.js
+++ src/screens/Map.js
@@ -1,19 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View, Text } from "react-native";
+import { View } from "react-native";
 
 export default function Map() {
   // Create Dummy Data With Array
-  const cars = ["BMW", "Mercedes-Benz", "Bugati", "Lexus"];
 
   return (
     <View>
       <StatusBar />
-      <Text>This is List On React</Text>
       {/* Code Here */}
-      {cars.map((car) => (
-        <Text key={car}>{car}</Text>
-      ))}
     </View>
   );
 }
