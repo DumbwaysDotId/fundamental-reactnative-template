538e0157d49a197eaa9e65cad8bbbef2c9c4f541
Your Name <samsulr99@gmail.com>
2022-09-08 09:23:32 +0700

    styling in reactnative

--- App.js
+++ App.js
@@ -1,15 +1,12 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
 import { View } from "react-native";
 
 //Import Screen
-import Form from "./src/screens/Form";
 
 export default function App() {
   return (
-    <View style={{ marginTop: 100 }}>
+    <View>
       <StatusBar />
-      <Form />
+      {/* Use Component */}
     </View>
   );
 }
